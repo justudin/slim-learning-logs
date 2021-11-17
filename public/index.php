3fccac2279d8fb24b58e431f79eec6934c9875a9
justudin <just.udin@yahoo.com>
2021-11-17 18:47:32 +0900

    api search with query return JSON data

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Slim\Factory\AppFactory;
 use DI\Container;
 require __DIR__ . '/../vendor/autoload.php';
@@ -21,14 +20,10 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->get('/', '\App\Controller\HomeController:homepage');
-
 $app->get('/hello/{name}', '\App\Controller\HomeController:hello');
-
 $app->get('/albums', '\App\Controller\SearchController:default');
-
 $app->get('/search', '\App\Controller\SearchController:search');
-
 $app->get('/form', '\App\Controller\SearchController:form');
 $app->post('/form', '\App\Controller\SearchController:form');
-
+$app->get('/api', '\App\Controller\ApiController:search');
 $app->run();
---
+++ src/Controller/ApiController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class ApiController extends BaseController
+{
+    public function search(Request $request, Response $response): Response
+    {
+        $albums = json_decode(file_get_contents(__DIR__ . '/../../data/albums.json'), true);
+
+        $query = strtolower($request->getQueryParam('q'));
+
+        $albums = $this->getAlbumByQuery($query, $albums);
+
+        return $response->withJson($albums);
+    }
+
+    private function getAlbumByQuery(string $query, $albums)
+    {
+        if ($query) {
+            $albums = array_values(array_filter($albums, function($album) use ($query) {
+                return str_contains(strtolower($album['title']), $query) || str_contains(strtolower($album['artist']), $query) !== false;
+            }));
+        }
+        return $albums;
+    }
+}
