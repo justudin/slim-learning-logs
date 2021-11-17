a8a764838ff9f35c4042c9b02edb4291313761ee
justudin <just.udin@yahoo.com>
2021-11-17 16:42:36 +0900

    Dependency Injection - PHP-DI - Template engine: mustache

--- project1/public/index.php
+++ project1/public/index.php
@@ -2,19 +2,22 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-
+use DI\Container;
 require __DIR__ . '/../vendor/autoload.php';
 
+$container =  new Container();
+
+$container->set('template', function (){
+    return new Mustache_Engine([
+        'loader' => new Mustache_Loader_FilesystemLoader(
+            __DIR__. '/../templates',
+            ['extension' => '']
+        )
+    ]);
+});
+
+AppFactory::setContainer($container);
+
 $app = AppFactory::create();
-
-$app->get('/', function(Request $request, Response $response){
-   $response->getBody()->write('Hello slim world!!');
-   return $response;
-});
-
-$app->get('/hello/{name}', function(Request $request, Response $response, array $args = []){
-    $response->getBody()->write('Hello '.$args['name'].' world!!');
-    return $response;
-});
 
 $app->run();
