78846ef44fc807a44b4f5189ba2cf85b55c9de3f
justudin <just.udin@yahoo.com>
2021-11-18 18:29:35 +0900

    custom error with template and status code

--- app/middlewares.php
+++ app/middlewares.php
@@ -7,6 +7,7 @@ use Slim\Exception\HttpNotFoundException;
 use Psr\Http\Message\ServerRequestInterface;
 use App\Controller\ExceptionController;
 use Slim\Views\TwigMiddleware;
+use Slim\Exception\HttpMethodNotAllowedException;
 
 return function (App $app){
     $container = $app->getContainer();
@@ -17,15 +18,24 @@ return function (App $app){
     $twig_settings = $container->get('template_twig');
     $app->add(TwigMiddleware::create($app, $twig_settings));
 
-    $settings = $app->getContainer()->get('settings');
+    $settings = $container->get('settings');
     $errorMiddleware = $app->addErrorMiddleware($settings['displayErrorDetails'], $settings['logErrorDetails'], $settings['logErrors']);
 
-    //custom error template
+    //custom 404 error template
     $errorMiddleware->setErrorHandler(
         HttpNotFoundException::class,
         function(ServerRequestInterface $request) use ($container){
             $exceptionController = new ExceptionController($container);
             return $exceptionController->notFound($request);
+        }
+    );
+
+    //custom 405 error template
+    $errorMiddleware->setErrorHandler(
+        HttpMethodNotAllowedException::class,
+        function(ServerRequestInterface $request) use ($container){
+            $exceptionController = new ExceptionController($container);
+            return $exceptionController->notAllowed($request);
         }
     );
 };
--- src/Controller/ExceptionController.php
+++ src/Controller/ExceptionController.php
@@ -9,6 +9,14 @@ class ExceptionController extends BaseController
     public function notFound(Request $request)
     {
         $response = new Response();
+        $response = $response->withStatus(404);
         return $this->render($response, '404-error.html');
+    }
+
+    public function notAllowed(Request $request)
+    {
+        $response = new Response();
+        $response = $response->withStatus(405);
+        return $this->render($response, '405-not-allowed.html');
     }
 }
