41dc5b8a94326d3a276226e48143a53694c9c5f5
justudin <just.udin@yahoo.com>
2021-11-18 10:50:23 +0900

    e-shop with details

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -9,7 +9,18 @@ class ShopController extends BaseController
 {
     public function default(Request $request, Response $response): Response
     {
-        $albums = json_decode(file_get_contents(__DIR__ . '/../../data/bikes.json'), true);
-        return $this->render($response, 'frontend/eshop.html', ['bikes' => $albums]);
+        $bikes = json_decode(file_get_contents(__DIR__ . '/../../data/bikes.json'), true);
+        return $this->render($response, 'frontend/eshop.html', ['bikes' => $bikes]);
+    }
+
+    public function details(Request $request, Response $response, array $args = []): Response
+    {
+        $bikes = json_decode(file_get_contents(__DIR__ . '/../../data/bikes.json'), true);
+
+        $bike_id = (int) $args['id'];
+
+        $key_data = array_search($bike_id, array_column($bikes, 'id'));
+
+        return $this->render($response, 'frontend/eshop-detail.html', ['bike' => $bikes[$key_data]]);
     }
 }
