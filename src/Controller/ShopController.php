b89a11fd23c56dd501a1e02c0746fbc23c9ff9e7
justudin <just.udin@yahoo.com>
2021-11-18 11:15:02 +0900

    data not found? throw 404 page.

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpNotFoundException;
 
 class ShopController extends BaseController
 {
@@ -20,6 +21,8 @@ class ShopController extends BaseController
         $bike_id = (int) $args['id'];
 
         $key_data = array_search($bike_id, array_column($bikes, 'id'));
+
+        if($key_data === false) throw new HttpNotFoundException($request, $response);
 
         return $this->render($response, 'frontend/eshop-detail.html', ['bike' => $bikes[$key_data]]);
     }
