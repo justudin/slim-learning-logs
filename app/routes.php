9714c29db204f9c93eab744138923964e57ebe47
justudin <just.udin@yahoo.com>
2021-11-18 16:28:27 +0900

    setName alias

--- app/routes.php
+++ app/routes.php
@@ -9,25 +9,26 @@ use App\Controller\ApiController;
 use App\Controller\ShopController;
 use App\Controller\AuthController;
 use App\Controller\DashboardController;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app){
 
     $app->get('/', [HomeController::class, 'homepage']);
-    $app->get('/hello/{name}', [HomeController::class,'hello']);
-    $app->get('/albums', [SearchController::class,'default']);
+    $app->get('/hello/{name}', [HomeController::class,'hello'])->setName('hello');
+    $app->get('/albums', [SearchController::class,'default'])->setName('album');
     $app->get('/search', [SearchController::class,'search']);
     $app->get('/form', [SearchController::class,'form']);
     $app->post('/form', [SearchController::class,'form']);
-    $app->get('/api', [ApiController::class, 'search']);
-    $app->get('/shop', [ShopController::class, 'default']);
-    $app->get('/shop/details/{id:[0-9]+}', [ShopController::class, 'details']);
-    $app->get('/login', [AuthController::class, 'login']);
+    $app->get('/api', [ApiController::class, 'search'])->setName('apisearch');
+    $app->get('/shop', [ShopController::class, 'default'])->setName('shop');
+    $app->get('/shop/details/{id:[0-9]+}', [ShopController::class, 'details'])->setName('shopdetails');
+    $app->get('/login', [AuthController::class, 'login'])->setName('login');
     $app->post('/login', [AuthController::class, 'loginAction']);
-    $app->post('/logout', [AuthController::class, 'logoutAction']);
+    $app->post('/logout', [AuthController::class, 'logoutAction'])->setName('logout');
 
-    $app->group('/dashboard', function($app){
-        $app->get('', [DashboardController::class, 'default']);
-        $app->get('/membership', [DashboardController::class, 'membership']);
+    $app->group('/dashboard', function(Group $group){
+        $group->get('', [DashboardController::class, 'default'])->setName('dashboard');
+        $group->get('/membership', [DashboardController::class, 'membership'])->setName('membership');
     })->add(new AuthMiddleware($app->getContainer()->get('session')));
 
 };
